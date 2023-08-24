779a23fb784e36204b5ba6036b931b682f382fdf
NyAndoMayah <hei.mayah.3@gmail.com>
2023-08-24 11:29:30 +0300

    chore: enterprise user cannot edit cnaps

---
+++ src/main/resources/db/migration/main/V0_9__insert_employee_table.sql
@@ -0,0 +1,6 @@
+-- Insert data into "user" table
+INSERT INTO "user" (username, password)
+VALUES
+    ('username_value_1', 'password_value_1'),
+    ('username_value_2', 'password_value_2'),
+    ('username_value_3', 'password_value_3');
